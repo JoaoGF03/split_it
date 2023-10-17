6a97283b4a08785d2d798246cac3f6216ac7d3ac
guilherme <joao.g.f@icloud.com>
2023-10-17 13:15:11 -0300

    feat: add firebase configuration & initialization

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,11 +1,64 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/login/login_page.dart';
+import 'package:split_it/modules/splash/splash_page.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_error) {
+      return const MaterialApp(
+        title: 'Split.it',
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              'Error initializing Firebase',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    if (!_initialized) {
+      return const MaterialApp(
+        title: 'Split.it',
+        home: SplashPage(),
+      );
+    }
+
     return const MaterialApp(
       title: 'Split.it',
       home: LoginPage(),
